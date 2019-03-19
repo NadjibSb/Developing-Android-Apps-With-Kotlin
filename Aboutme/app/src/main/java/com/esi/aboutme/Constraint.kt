218b897740bad78f5f14c3bb6393f592c285b9c5
NadjibSb <fn_souab@esi.dz>
2019-03-19 02:44:59 +0100

    creating the 'AndroidTrivia' app

--- Aboutme/app/src/main/java/com/esi/aboutme/Constraint.kt
+++ Aboutme/app/src/main/java/com/esi/aboutme/Constraint.kt
@@ -1,7 +1,7 @@
 package com.esi.aboutme
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.constraint_layout.*
 
@@ -14,30 +14,30 @@ class Constraint : AppCompatActivity() {
     }
 
     private fun setListners() {
-        val views: List<View> = listOf(textView,textView2,textView3,textView4,textView5,textView6,textView7)
+        val views: List<View> = listOf(textView, textView2, textView3, textView4, textView5, textView6, textView7)
 
-        for(v in views){
+        for (v in views) {
             v.setOnClickListener { makeColor(it) }
         }
 
         reset_btn.setOnClickListener { reset(views) }
     }
 
-    private fun reset(views: List<View> ) {
-        for (v in views){
+    private fun reset(views: List<View>) {
+        for (v in views) {
             v.setBackgroundResource(android.R.color.holo_green_light)
         }
     }
 
     private fun makeColor(view: View) {
-        when(view.id){
-            R.id.textView ->view.setBackgroundResource(android.R.color.holo_blue_bright)
-            R.id.textView2 ->view.setBackgroundResource(android.R.color.holo_green_dark)
-            R.id.textView3 ->view.setBackgroundResource(android.R.color.holo_red_dark)
-            R.id.textView4 ->view.setBackgroundResource(android.R.color.holo_blue_light)
-            R.id.textView5 ->view.setBackgroundResource(android.R.color.holo_orange_light)
-            R.id.textView6 ->view.setBackgroundResource(android.R.color.holo_red_light)
-            R.id.textView7 ->view.setBackgroundResource(android.R.color.holo_purple)
+        when (view.id) {
+            R.id.textView -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+            R.id.textView2 -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.textView3 -> view.setBackgroundResource(android.R.color.holo_red_dark)
+            R.id.textView4 -> view.setBackgroundResource(android.R.color.holo_blue_light)
+            R.id.textView5 -> view.setBackgroundResource(android.R.color.holo_orange_light)
+            R.id.textView6 -> view.setBackgroundResource(android.R.color.holo_red_light)
+            R.id.textView7 -> view.setBackgroundResource(android.R.color.holo_purple)
             else -> view.setBackgroundResource(android.R.color.background_light)
         }
     }
